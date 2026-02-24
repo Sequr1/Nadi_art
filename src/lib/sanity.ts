8f5557d3e4c75c7533b07218d04db82fcaa0748d
searchernov <119491559+Sequr1@users.noreply.github.com>
2026-02-24 15:30:45 +0300

    Update sanity.ts

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -6,7 +6,7 @@ export const sanityConfig = {
  projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
 dataset: import.meta.env.VITE_SANITY_DATASET,
   apiVersion: '2024-01-01',
-  useCdn: true,
+  useCdn: false,
 };
 
 export const sanityClient = createClient(sanityConfig);
